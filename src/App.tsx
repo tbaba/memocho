f6e5c8a640476d8c7c69ee528d9abb0907a7c4fa
Tatsuro Baba <harakirisoul@gmail.com>
2021-05-02 19:11:43 +0900

    Set the initial memo for local testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import { RecoilRoot } from 'recoil';
-import { Container } from './components';
+import React from "react";
+import { RecoilRoot, MutableSnapshot } from "recoil";
+import { Container } from "./components";
+import { editMemoState, getNewId, memosState } from "./modules/memos";
 
 function App() {
   return (
-    <RecoilRoot>
+    <RecoilRoot initializeState={initialize}>
       <Container />
     </RecoilRoot>
   );
 }
+
+const initialize = ({ set }: MutableSnapshot) => {
+  const initialMemo = {
+    id: getNewId(),
+    title: "最初のやつ",
+    body: "最初のやつ。\n最初のやつやで。",
+  };
+
+  set(memosState, [initialMemo]);
+  set(editMemoState, initialMemo);
+};
 
 export default App;
