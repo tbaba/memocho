43da68361edf6f5dfa000531e834e876426014f1
Tatsuro Baba <harakirisoul@gmail.com>
2021-05-03 16:35:47 +0900

    Enable to sign-in/out to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,76 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { RecoilRoot, MutableSnapshot } from "recoil";
+import Amplify, { Auth } from "aws-amplify";
+import { AuthState, CognitoUserInterface, onAuthUIStateChange } from "@aws-amplify/ui-components";
+import { AmplifyAuthenticator, AmplifySignUp } from "@aws-amplify/ui-react";
+import awsconfigure from './aws-exports';
+
 import { Container } from "./components";
 import { editMemoState, getNewId, memosState } from "./modules/memos";
+import { AppBar, IconButton, Toolbar, Typography } from "@material-ui/core";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 
-function App() {
-  return (
+function useApp(): AppProps {
+  const [authState, setAuthState] = useState<AuthState | undefined>();
+  const [user, setUser] = useState<CognitoUserInterface | undefined>();
+
+  return {
+    authState,
+    user,
+    setAuthState,
+    setUser,
+  }
+}
+
+type AppProps = {
+  authState: AuthState | undefined;
+  user: CognitoUserInterface | undefined;
+  setAuthState: (nextAuthState: AuthState) => void;
+  setUser: (authData: CognitoUserInterface) => void;
+}
+
+Amplify.configure(awsconfigure);
+
+function App({ authState, user, setAuthState, setUser }: AppProps) {
+
+  useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState as AuthState);
+      setUser(authData as CognitoUserInterface);
+    })
+  }, []);
+
+  const handleSignOut = () => {
+    Auth.signOut();
+  }
+
+  return authState === AuthState.SignedIn && user ? (
     <RecoilRoot initializeState={initialize}>
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6" noWrap>
+            Memocho
+          </Typography>
+          <div onClick={handleSignOut}>
+            <IconButton aria-label="display more actions" edge="end" color="inherit">
+              <ExitToAppIcon />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
       <Container />
     </RecoilRoot>
+  ) : (
+    <AmplifyAuthenticator>
+      <AmplifySignUp
+        slot="sign-up"
+        formFields={[
+          { type: "username" },
+          { type: "password" },
+          { type: "email" },
+        ]}
+      />
+    </AmplifyAuthenticator>
   );
 }
 
@@ -22,4 +85,6 @@ const initialize = ({ set }: MutableSnapshot) => {
   set(editMemoState, initialMemo);
 };
 
-export default App;
+export default function() {
+  return <App {...useApp()} />;
+}
